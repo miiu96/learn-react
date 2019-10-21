8515fed1df188824ef9bea68560dbd74881cf585
Iuga Mihai <mihai9625@gmail.com>
2019-10-21 21:32:16 +0300

    list of items

--- src/App.js
+++ src/App.js
@@ -4,35 +4,10 @@ import Container1 from './containers/container1'
 
 class App extends Component {
   
-  rendeFunction1 = () =>{
-    if(true) {
-      return(
-        <div>
-          Condition 1
-        </div>
-      )
-    }
-    else {
-      return(
-        <div>
-          Condition 2
-        </div>
-      )
-    }
-  }
-
   render() {
     return(
       <div>
-        React
-        <div>
-          Div 1 
-          {this.rendeFunction1()}
-        </div>
-        <div>
-          Div 2
-          {this.rendeFunction1()}
-        </div>
+        <Container1/>
       </div>
      
     )
--- src/containers/container1.js
+++ src/containers/container1.js
@@ -1,43 +1,25 @@
 import React, {Component} from 'react';
-import Component1 from '../functional/component1'
 
 class Cotainer1 extends Component {
-    constructor(props) {
-        super(props)
+    Arr1 = [
+        {id:1, text: 'text 1', number: 1},
+        {id:2, text: 'text 2', number: 2},
+        {id:3, text: 'text 3', number: 3},
+        {id:4, text: 'text 4', number: 4},
+        {id:5, text: 'text 5', number: 5}
+    ]
 
-        this.state = {
-            stateprop1: "Our initial state",
-            stateprop2: 5
-        }
-    }
-
-    changeState = () => (
-        this.setState({ 
-            stateprop2: this.state.stateprop2 + 1,
-            stateprop1: this.state.stateprop1 + "L"
-        })
+    renderListItem = (props) => (
+        <div>
+            {props.item.text}
+            <p>{props.item.number}</p>
+        </div>
     )
-
-    changeState2 = () => (
-        this.setState({ stateprop1: this.state.stateprop1 + "L",
-        stateprop2: this.state.stateprop2 + 1,
-        })
-    )
-
 
  render(){
     return(
      <div>
-         <button onClick={() => this.changeState()}> change state</button>
-         {this.state.stateprop2}
-         <br/>
-         <button onClick={() => this.changeState2()}> change state2</button>
-         <br/>
-         {this.state.stateprop1}
-         <br/>
-         <Component1 prop1= {this.state.stateprop1}/>
-         <br/>
-         
+        {this.Arr1.map((item, index) => (<this.renderListItem key={item.id} item={item} />))}    
      </div>
     )
   }
